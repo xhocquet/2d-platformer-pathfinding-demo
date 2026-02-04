461265e802b2eeaaed0584a321d4545a8d23e6bb
xhocquet <xhocquet@gmail.com>
2026-02-03 21:18:17 -0500

    Lead in jump

--- enemy.gd
+++ enemy.gd
@@ -1,9 +1,10 @@
 extends "res://player.gd"
 
 enum AiState { STANDING, TRAVERSING_EDGE, JUMP_SEQUENCE }
-enum JumpPhase { JUMP_PRESS, ASCENT, AIR_CONTROL }
+enum JumpPhase { LEAD_IN, JUMP_PRESS, ASCENT, AIR_CONTROL }
 
 const ARRIVAL_DIST: float = 16.0
+const LEAD_IN_DIST: float = 10.0  # must be this close to source node before initiating jump
 const JUMP_AIR_CONTROL_HEIGHT_RATIO: float = 0.7
 
 @export var speed := 100.0
@@ -19,15 +20,12 @@ var _jump_phase: JumpPhase = JumpPhase.JUMP_PRESS
 var _jump_start_y: float = 0.0
 var _jump_peak_y: float = 0.0
 var _jump_back_out_dir: float = 0.0  # if inside ledge, move this way until peak (-1, 0, 1)
+var _lead_in_target_x: float = 0.0  # takeoff edge x we walk toward during LEAD_IN
 
 func _ready() -> void:
 	_player = get_parent().get_node("Player") as CharacterBody2D
 	_graph = (get_parent().get_node("SectionGraph") as Node2D).graph
 	call_deferred("_initialize_ai_state")
-
-func _physics_process(delta: float) -> void:
-	super._physics_process(delta)
-	_process_ai(delta)
 
 func _initialize_ai_state() -> void:
 	var from_id: StringName = _graph.get_section_under_body(self)
@@ -40,6 +38,20 @@ func _initialize_ai_state() -> void:
 		_ai_state = AiState.TRAVERSING_EDGE
 		_source_node = _path[0]
 		_dest_node = _path[1]
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	_process_ai(delta)
+
+func _process_ai(_delta: float) -> void:
+	_update_current_section()
+	match _ai_state:
+		AiState.STANDING:
+			_tick_standing()
+		AiState.TRAVERSING_EDGE:
+			_tick_traversing_edge()
+		AiState.JUMP_SEQUENCE:
+			_tick_jump_sequence()
 
 func _get_move_input() -> float:
 	# Walking towards a node
@@ -57,12 +69,14 @@ func _get_move_input() -> float:
 	return 0.0
 
 func _get_jump_sequence_move_input() -> float:
+	if _jump_phase == JumpPhase.LEAD_IN:
+		return signf(_lead_in_target_x - global_position.x)
+
 	if _jump_back_out_dir != 0.0 and (_jump_phase == JumpPhase.JUMP_PRESS or _jump_phase == JumpPhase.ASCENT):
 		return _jump_back_out_dir
 
-	if _jump_phase == JumpPhase.AIR_CONTROL and _has_dest_node():
-		var target_pos: Vector2 = _graph.get_section_position(_dest_node)
-		return signf(target_pos.x - global_position.x)
+	if _jump_phase == JumpPhase.AIR_CONTROL:
+		return signf(_player.global_position.x - global_position.x)
 
 	return 0.0
 
@@ -82,16 +96,6 @@ func _calculate_path() -> Array[StringName]:
 		return []
 	var path: Array[StringName] = _graph.find_path(from_id, to_id)
 	return path
-
-func _process_ai(_delta: float) -> void:
-	_update_current_section()
-	match _ai_state:
-		AiState.STANDING:
-			_tick_standing()
-		AiState.TRAVERSING_EDGE:
-			_tick_traversing_edge()
-		AiState.JUMP_SEQUENCE:
-			_tick_jump_sequence()
 
 func _update_current_section() -> void:
 	if _ai_state == AiState.TRAVERSING_EDGE and _source_node != &"" and _has_dest_node():
@@ -139,10 +143,12 @@ func _start_jump_sequence(edge_from: StringName, edge_to: StringName) -> void:
 	_source_node = edge_from
 	_dest_node = edge_to
 	_ai_state = AiState.JUMP_SEQUENCE
-	_jump_phase = JumpPhase.JUMP_PRESS
+	_jump_phase = JumpPhase.LEAD_IN
 	_jump_start_y = global_position.y
 	_jump_peak_y = global_position.y
-	_wants_jump_press = true
+	_wants_jump_press = false
+	_jump_back_out_dir = 0.0
+	# Lead-in walks toward the takeoff edge (our platform’s edge toward the dest), not the section position
 	var pos_to: Vector2 = _graph.get_section_position(edge_to)
 	var ledge_min_x: float = pos_to.x
 	var ledge_max_x: float = pos_to.x
@@ -155,15 +161,36 @@ func _start_jump_sequence(edge_from: StringName, edge_to: StringName) -> void:
 	var pos_from: Vector2 = _graph.get_section_position(edge_from)
 	var ledge_center_x: float = (ledge_min_x + ledge_max_x) * 0.5
 	var approach_from_left: bool = pos_from.x < ledge_center_x
-	var edge_x: float = ledge_min_x if approach_from_left else ledge_max_x
-	var inside: bool = (approach_from_left and global_position.x >= edge_x) or (not approach_from_left and global_position.x <= edge_x)
-	if inside:
-		_jump_back_out_dir = -1.0 if approach_from_left else 1.0
-	else:
-		_jump_back_out_dir = 0.0
+	var platform_other_x: float = pos_from.x
+	for nb in _graph.get_neighbors(edge_from):
+		if nb.type == SectionGraph.EdgeType.WALK:
+			platform_other_x = _graph.get_section_position(nb.to).x
+			break
+	_lead_in_target_x = maxf(pos_from.x, platform_other_x) if approach_from_left else minf(pos_from.x, platform_other_x)
 
 func _tick_jump_sequence() -> void:
-	if _jump_phase == JumpPhase.JUMP_PRESS:
+	if _jump_phase == JumpPhase.LEAD_IN:
+		if absf(global_position.x - _lead_in_target_x) <= LEAD_IN_DIST:
+			_jump_phase = JumpPhase.JUMP_PRESS
+			_wants_jump_press = true
+			_jump_start_y = global_position.y
+			_jump_peak_y = global_position.y
+			var pos_to: Vector2 = _graph.get_section_position(_dest_node)
+			var ledge_min_x: float = pos_to.x
+			var ledge_max_x: float = pos_to.x
+			for nb in _graph.get_neighbors(_dest_node):
+				if nb.type == SectionGraph.EdgeType.WALK:
+					var pos_nb: Vector2 = _graph.get_section_position(nb.to)
+					ledge_min_x = minf(pos_to.x, pos_nb.x)
+					ledge_max_x = maxf(pos_to.x, pos_nb.x)
+					break
+			var pos_from: Vector2 = _graph.get_section_position(_source_node)
+			var ledge_center_x: float = (ledge_min_x + ledge_max_x) * 0.5
+			var approach_from_left: bool = pos_from.x < ledge_center_x
+			var edge_x: float = ledge_min_x if approach_from_left else ledge_max_x
+			var inside: bool = (approach_from_left and global_position.x >= edge_x) or (not approach_from_left and global_position.x <= edge_x)
+			_jump_back_out_dir = -1.0 if (inside and approach_from_left) else (1.0 if (inside and not approach_from_left) else 0.0)
+	elif _jump_phase == JumpPhase.JUMP_PRESS:
 		if not is_on_floor():
 			_jump_phase = JumpPhase.ASCENT
 			_jump_peak_y = global_position.y
